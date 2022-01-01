ac17c721aa4b27dbcde39131143039193041b9a5
Tarek Hasabelnaby <tarek@Tareks-MacBook-Pro.local>
2022-01-01 07:34:57 -0500

    Add dummy events data

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,9 +1,9 @@
 import { useRouter } from "next/router";
 
-export default function CollectEvents() {
+export default function FilteredEvents() {
   const router = useRouter();
   console.log(Object(router.query));
   // API call w\ eventID and then display data to the user
 
-  return <div>{router.query.eventId}</div>;
+  return <div>Filtered Events</div>;
 }
